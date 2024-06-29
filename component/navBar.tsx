b1c32778be2a22f39e607ad41e6694f5434a9606
lilkaLePro <kaly100diallo@gmail.com>
2024-06-29 11:13:46 +0100

    ajout Logo | changement metadonnés

--- component/navBar.tsx
+++ component/navBar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import clsx from "clsx";
+import Image from "next/image";
 import Link from "next/link";
 import {  usePathname } from "next/navigation";
 
@@ -15,7 +16,8 @@ export const NavBar = () => {
   
     return (<nav className="h-24  flex justify-around items-center max-md:hidden ">
       <div>
-        <h1 className="font-semibold">JEMACODER</h1>
+        {/* <h1 className="font-semibold">JEMACODER</h1> */}
+        <div><Image src="/jemacoderLogo.png" width={100} height={20} alt="logo jemacoder" className="rounded-md" /> </div>
       </div>
       <div>
         <ul className="flex gap-10">
