35421aa2fdf98a902e5d4b37ac0cf0e2a3e7f1c5
lilkaLePro <kaly100diallo@gmail.com>
2024-07-08 16:22:30 +0100

    ajout de la sideBar de la page dashboard |  ajout de de la top bar

--- app/(organisateur)/dashboard/layout.tsx
+++ app/(organisateur)/dashboard/layout.tsx
@@ -1,14 +1,17 @@
-import { NavBar } from "@/component/dashboards/NavBar";
+import { Container } from "@/component/container";
+import { SideBar } from "@/component/dashboards/NavBar";
 
 export default function Layout ({children} : {children : React.ReactNode}) {
 
-    return (
-        <div className="flex">
+    return (<>
+        <Container className="max-w-full bg-muted flex">
             <div>
-                <NavBar />
+                <SideBar />
             </div>
-            <div>{children} </div>
-            <div></div>
-        </div>
-    )
+            <div className="w-full">{children} </div>
+            <div>
+
+            </div>
+        </Container>
+        </>)
 }
--- component/dashboards/NavBar.tsx
+++ component/dashboards/NavBar.tsx
@@ -1,11 +1,23 @@
 import { NavLinks } from "./navLinks"
+import { Profil } from "./profil"
 
-export const NavBar = () => {
-    return (<div>
-        <div></div>
+export const SideBar = () => {
+    return (<div className="h-screen max-w-60 flex flex-col justify-between bg-white">
         <div>
+            <div className="h-20 w-full border ">
+
+            </div>
             <NavLinks />
         </div>
-        <div></div>
+        <div>
+            <Profil />
+        </div>
     </div>)
+}
+export const NavBar = () => {
+    return (
+        <>
+
+        </>
+    )
 }
