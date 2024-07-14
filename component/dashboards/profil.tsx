58ceb9bcd64de305a2efc8c51d0bd5f65ab6bb14
lilkaLePro <kaly100diallo@gmail.com>
2024-07-14 11:34:01 +0100

    j'ai changer le composant NavLink de uncontrolled en un controlled list items

--- component/dashboards/profil.tsx
+++ component/dashboards/profil.tsx
@@ -1,5 +1,9 @@
 import { BellIcon } from "@radix-ui/react-icons"
 import { Avatar } from "../avatar"
+const profil = {
+    name : "jeemacoder team",
+    email : "kaly100diallo@gmail.com"
+}
 
 export const Profil = () => {
     return <div className="h-40 bg-muted m-3 p-3 justify-between flex flex-col rounded-md">
@@ -7,6 +11,6 @@ export const Profil = () => {
         <span className="text-xs">notificatios</span> 
         <BellIcon /> 
     </div>
-    <Avatar name="Kaly Diallo" email="lilkaExample@gmail.com"/>
+    <Avatar profil={profil}/>
     </div>
 }
